4b4f7fa8112f090028e365383faf25c743b78495
夏冰123 <2479191371@qq.com>
2017-10-27 00:01:19 +0800

    '第二天提交'

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,22 @@ $(function(){
 	$('#i1').click(function(){
 		a++;
 		console.log(a);
-		$('.img1').eq(a).addClass('img2').siblings().removeClass('img2');
-		// $('.img1').eq(a).addClass('img3').removeClass('img3');
+		$('.img1').eq(a).css('opacity','1');
+		
 	});
+
+	$('.nav-search1 ul li').click(function(){
+		for(var i = 0;i < 4;i++){
+			var a = $(this).index();
+			$('.nav-search2 form').eq(a).css('display','block').siblings().css('display','none');
+		}
+	})
+	
+	$('form input').on('focus',function(){
+		$('dl').css('display','block');
+	// 	// alert(1);
+	})
+	$('form input').on('blur',function(){
+		$('dl').css('display','none');
+	})
 })
